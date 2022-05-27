fd23bbe4debb74ea4d283d7ce3a0ede95975a95f
Hemanth-Kumar-Rajoli <hemanthkumarrajoli@gmail.com>
2022-05-28 00:41:34 +0530

    added cards

--- Routers/myRoutes.js
+++ Routers/myRoutes.js
@@ -3,7 +3,7 @@ const ejs = require('ejs')
 const router = express.Router()
 
 router.route('/').get((req,res)=>{
-    res.render('index')
+    res.render('index',{skills:["HTML","JS","PYTHON","JAVA"]});
 })
 
 module.exports = router
