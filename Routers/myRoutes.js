d690228b0942c1bf9bfd3693ccb0bf8ed1d1a9b1
Hemanth-Kumar-Rajoli <hemanthkumarrajoli@gmail.com>
2022-06-07 14:51:53 +0530

    upgraded mobile responsive to the skills cards

--- Routers/myRoutes.js
+++ Routers/myRoutes.js
@@ -3,7 +3,7 @@ const ejs = require('ejs')
 const router = express.Router()
 
 router.route('/').get((req,res)=>{
-    res.render('index',{skills:["HTML","JS","PYTHON","JAVA","CSS","REACT","node.js","MySql"]});
+    res.render('index',{skills:["JS","node.js","REACT","JAVA","PYTHON","MySql","HTML","CSS"],descriptions:{"JS":"The first language I experienced with OOP's concept with minimal doubts on OOP'S concept raised by python language. ","node.js":'djfajb',"REACT":'dsakfjbadsf',"JAVA":'kajsfdasdgasdf',"PYTHON":'The langua',"MySql":'jhafkjhasdfjhdsf',"HTML":'Commenced with enthusiasm to learn and do something big, I did it. The amusing moment here is I used to code in pure HTML format as a kiddy coder, and now HTML is altered with EJS and react. My moment with HTML is like coding is easy but alignment is tricky.',"CSS":'CSS The "make-up man" of HTML. Learning is easy, applying needs experience to arrange all things proper and good looking. '}});
 })
 
 module.exports = router
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,38 +1,50 @@
+let itemNo = 0
+document.querySelector(`[data-item-no='0']`).classList.add('skills-card-hide');
+function hideOnMobileCompatability(number,boolHide){
+    console.log(number)
+    let ele = document.querySelector(`[data-item-no='${number}']`)
+    if(boolHide){
+        ele.classList.remove('skills-card-hide');
+    }else{
+        ele.classList.add('skills-card-hide');
+    }
+    console.log(ele)
+}
 document.querySelector('#backward').addEventListener('click',()=>{
     rightPosition = document.getElementById('skills-card-container')
     rightInteger = parseInt((rightPosition.style.right.split("px").length>0)?rightPosition.style.right.split("px")[0]:'0');
     if(rightPosition.style.right && rightInteger>0){
-        rightPosition.style.right=""+(rightInteger-300)+"px";
+        rightPosition.style.right=""+(rightInteger-310)+"px";
     }
 })
 document.querySelector('#forward').addEventListener('click',()=>{
     rightPosition = document.getElementById('skills-card-container')
     if(!rightPosition.style.right){
-        rightPosition.style.right='300px'
+        rightPosition.style.right='310px'
     }
     else{
-        if(parseInt(rightPosition.style.right.split("px")[0])>=(rightPosition.dataset.totalSkills*300))
+        if(parseInt(rightPosition.style.right.split("px")[0])>=(rightPosition.dataset.totalSkills*310)){
             rightPosition.style.right="0px"
-        else
-            rightPosition.style.right=""+(parseInt(rightPosition.style.right.split("px")[0])+300)+"px";
+        }
+        else{
+            rightPosition.style.right=""+(parseInt(rightPosition.style.right.split("px")[0])+310)+"px";
+        }
     }
 })
 document.querySelector('#backward-2').addEventListener('click',()=>{
     rightPosition = document.getElementById('skills-card-container')
-    rightInteger = parseInt((rightPosition.style.right.split("px").length>0)?rightPosition.style.right.split("px")[0]:'0');
-    if(rightPosition.style.right && rightInteger>0){
-        rightPosition.style.right=""+(rightInteger-300)+"px";
+    // rightInteger = parseInt((rightPosition.style.right.split("px").length>0)?rightPosition.style.right.split("px")[0]:'0');
+    if(itemNo>0){
+        // rightPosition.style.right=""+(rightInteger-310)+"px";
+        hideOnMobileCompatability(itemNo,true);
+        itemNo--;
+        hideOnMobileCompatability(itemNo,false);
     }
 })
 document.querySelector('#forward-2').addEventListener('click',()=>{
     rightPosition = document.getElementById('skills-card-container')
-    if(!rightPosition.style.right){
-        rightPosition.style.right='300px'
-    }
-    else{
-        if(parseInt(rightPosition.style.right.split("px")[0])>=(rightPosition.dataset.totalSkills*300))
-            rightPosition.style.right="0px"
-        else
-            rightPosition.style.right=""+(parseInt(rightPosition.style.right.split("px")[0])+300)+"px";
-    }
+    hideOnMobileCompatability(itemNo,true);
+    
+    itemNo=(itemNo+1)%(rightPosition.dataset.totalSkills);
+    hideOnMobileCompatability(itemNo,false);
 })
